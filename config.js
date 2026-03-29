279f52d1386750ae98aea718a9b313466aa3395e
Mohammad Faiz <mohdfaiz2k9@gmail.com>
2026-03-29 22:22:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ window.CONFIG = {
     SEARCH_DEBOUNCE_MS: 300,
     DEFAULT_CLASS: 11,
     DOWNLOAD_PATHS: {
-        11: 'Mohammad-Faiz-Cloud-Engineer/Vibrant-Academy/releases/download/V1.0.0/Class.11th.Modules.zip',
-        12: 'Mohammad-Faiz-Cloud-Engineer/Vibrant-Academy/releases/download/V1.0.0/Class.12th.Modules.zip'
+        11: 'https://github.com/Mohammad-Faiz-Cloud-Engineer/Vibrant-Academy/releases/download/V1.0.0/Class.11th.Modules.zip',
+        12: 'https://github.com/Mohammad-Faiz-Cloud-Engineer/Vibrant-Academy/releases/download/V1.0.0/Class.12th.Modules.zip'
     }
 };
 
