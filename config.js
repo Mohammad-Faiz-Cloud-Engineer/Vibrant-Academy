91dd102b0e640ad54873c734e615b6298bbb0800
Mohammad Faiz <mohdfaiz2k9@gmail.com>
2026-04-03 07:11:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,24 @@
 window.CONFIG = {
     SEARCH_DEBOUNCE_MS: 300,
     DEFAULT_CLASS: 11,
+    // GitHub repository info for accessing media files
+    GITHUB_REPO: 'God-Hand1/Vibrant-Academy',
+    GITHUB_BRANCH: 'main',
+    // Check if running on GitHub Pages (production)
+    IS_PRODUCTION: window.location.hostname.includes('github.io'),
     DOWNLOAD_PATHS: {
         11: 'https://github.com/Mohammad-Faiz-Cloud-Engineer/Vibrant-Academy/releases/download/V1.0.0/Class.11th.Modules.zip',
-        12: 'https://github.com/Mohammad-Faiz-Cloud-Engineer/Vibrant-Academy/Vibrant-Academy/releases/download/V1.0.0/Class.12th.Modules.zip',
-        'music': 'https://github.com/Mohammad-Faiz-Cloud-Engineer/Vibrant-Academy/Vibrant-Academy/releases/download/V1.0.0/Songs.zip'
+        12: 'https://github.com/Mohammad-Faiz-Cloud-Engineer/Vibrant-Academy/releases/download/V1.0.0/Class.12th.Modules.zip',
+        'music': 'https://github.com/Mohammad-Faiz-Cloud-Engineer/Vibrant-Academy/releases/download/V1.0.0/Songs.zip'
+    },
+    // Helper function to get correct file URL
+    getFileUrl: function(relativePath) {
+        if (this.IS_PRODUCTION) {
+            // In production (GitHub Pages), access files from raw GitHub content
+            return `https://raw.githubusercontent.com/${this.GITHUB_REPO}/${this.GITHUB_BRANCH}/${encodeURI(relativePath)}`;
+        }
+        // In development (localhost), use relative paths
+        return relativePath;
     }
 };
 
